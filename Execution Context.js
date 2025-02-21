fab632e8efb656b31b9197a0c810fed53329817f
Husnain555 <husnain.babar.dev@gmail.com>
2025-02-21 13:55:37 +0500

    execution context

---
+++ Execution Context.js
@@ -0,0 +1,5 @@
+// In the execution context, there are two portions: one is called the memory component, and the other is called the code component.
+//
+//     The memory component, also known as the variable environment, stores all objects and function objects in a key-value pair format.
+//
+//     The code component is where JavaScript executes these objects and functions line by line. This is why JavaScript is called an asynchronous language.
