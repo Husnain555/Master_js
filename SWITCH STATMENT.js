98e0e63dc676acb35a8c92f328eef38fd0a8d036
Husnain555 <husnain.babar.dev@gmail.com>
2025-02-25 15:58:19 +0500

    execution context

---
+++ SWITCH STATMENT.js
@@ -0,0 +1,11 @@
+const fruitType = "Banana"
+switch (fruitType){
+    case "orange":
+        console.log("not fruit typ")
+        break;
+    case "Banana":
+        console.log("fruit matched")
+        break;
+    default :
+        console.log("no fruit found")
+}
---
+++ condition statments.js
@@ -0,0 +1,15 @@
+// if statment
+const x = 2
+if (x === 2){
+    console.log("x is 2")
+} else {
+    console.log("x is not 2")
+}
+
+console.log(typeof [])
+
+if (!Boolean({})){
+    console.log("value is falsy" )
+}else {
+    console.log("value not false")
+}
