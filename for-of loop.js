079f5f2d0300a47a5abcd7f49cf7f1d0e049970f
Husnain555 <husnain.babar.dev@gmail.com>
2025-03-17 22:33:33 +0500

    for of

--- for-of loop.js
+++ for-of loop.js
@@ -1,5 +1,31 @@
-const array = [1, 2, 3, 4, 5];
+// const array = [1, 2, 3, 4, 5];
+// for(let i = 0;i<array.length;i++){
+//     console.log(array[i]*2)
+//
+// }
 
-for (let i = 0; i < 10; i++) {
-    console.log(array[i % array.length]); // Loops through array using modulo
+// let fruits = ["Apple", "Banana", "Mango"];
+// for(let i= 0 ;i<fruits.length; i++){
+//     console.log(fruits[i].length)
+// }
+// const Input = [3, 4, -1, 1]
+//
+// for(let i = 0;i<Input.length; i++){
+//     console.log(Math.min(...Input));
+// }
+
+// find the missing numbers
+const num = [1,4,3,8]
+function missing(num){
+    const n = Math.max(...num)
+    const newMissing = new Set(num);
+    let missing = [];
+    for(let i = 0;i<=n; i++){
+        if(!newMissing.has(i)){
+            missing.push(i)
+        }
+    }
+    return missing
+
 }
+console.log(missing(num))
